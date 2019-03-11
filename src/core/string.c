156823252943437288136090c6ccf06da6612f31
J.-F. Cap <jfc@seco.be>
2019-03-11 02:06:35 +0100

    Added string/match lua style pattern matching function

--- src/core/string.c
+++ src/core/string.c
@@ -20,6 +20,7 @@
 * IN THE SOFTWARE.
 */
 
+#include <ctype.h>
 #include <string.h>
 
 #ifndef JANET_AMALG
@@ -447,6 +448,449 @@ static Janet cfun_string_format(int32_t argc, Janet *argv) {
     return janet_stringv(buffer->data, buffer->count);
 }
 
+/*
+ * code adapted from lua/lstrlib.c http://lua.org
+ */
+
+
+#define CAP_UNFINISHED  (-1)
+#define CAP_POSITION    (-2)
+
+#define MAXCAPTURES 256
+#define MAXCCALLS 200
+#define CHAR_ESC        '%'
+#define SPECIALS    "^$*+?.([%-"
+
+#define uchar(c)    ((unsigned char)(c))
+
+typedef struct MatchState {
+    const char *src_init;  /* init of source string */
+    const char *src_end;  /* end ('\0') of source string */
+    const char *p_end;  /* end ('\0') of pattern */
+    int matchdepth;  /* control for recursive depth (to avoid C stack overflow) */
+    unsigned char level;  /* total number of captures (finished or unfinished) */
+    struct {
+        const char *init;
+        ptrdiff_t len;
+    } capture[MAXCAPTURES];
+} MatchState;
+
+
+/* recursive function */
+static const char *match(MatchState *ms, const char *s, const char *p);
+
+static int check_capture(MatchState *ms, int l) {
+    l -= '1';
+    if (l < 0 || l >= ms->level || ms->capture[l].len == CAP_UNFINISHED)
+        janet_panicf("invalid capture index %%%d", l + 1);
+    return l;
+}
+
+static int capture_to_close(MatchState *ms) {
+    int level = ms->level;
+    for (level--; level >= 0; level--)
+        if (ms->capture[level].len == CAP_UNFINISHED) return level;
+    janet_panic("invalid pattern capture");
+    return 0;
+}
+
+
+static const char *classend(MatchState *ms, const char *p) {
+    switch (*p++) {
+        case CHAR_ESC: {
+            if (p == ms->p_end)
+                janet_panic("malformed pattern (ends with '%%')");
+            return p + 1;
+        }
+        case '[': {
+            if (*p == '^') p++;
+            do {  /* look for a ']' */
+                if (p == ms->p_end)
+                    janet_panic("malformed pattern (missing ']')");
+                if (*(p++) == CHAR_ESC && p < ms->p_end)
+                    p++;  /* skip escapes (e.g. '%]') */
+            } while (*p != ']');
+            return p + 1;
+        }
+        default: {
+            return p;
+        }
+    }
+}
+
+
+static int match_class(int c, int cl) {
+    int res;
+    switch (tolower(cl)) {
+        case 'a' :
+            res = isalpha(c);
+            break;
+        case 'c' :
+            res = iscntrl(c);
+            break;
+        case 'd' :
+            res = isdigit(c);
+            break;
+        case 'g' :
+            res = isgraph(c);
+            break;
+        case 'l' :
+            res = islower(c);
+            break;
+        case 'p' :
+            res = ispunct(c);
+            break;
+        case 's' :
+            res = isspace(c);
+            break;
+        case 'u' :
+            res = isupper(c);
+            break;
+        case 'w' :
+            res = isalnum(c);
+            break;
+        case 'x' :
+            res = isxdigit(c);
+            break;
+        case 'z' :
+            res = (c == 0);
+            break;  /* deprecated option */
+        default:
+            return (cl == c);
+    }
+    return (islower(cl) ? res : !res);
+}
+
+
+static int matchbracketclass(int c, const char *p, const char *ec) {
+    int sig = 1;
+    if (*(p + 1) == '^') {
+        sig = 0;
+        p++;  /* skip the '^' */
+    }
+    while (++p < ec) {
+        if (*p == CHAR_ESC) {
+            p++;
+            if (match_class(c, uchar(*p)))
+                return sig;
+        } else if ((*(p + 1) == '-') && (p + 2 < ec)) {
+            p += 2;
+            if (uchar(*(p - 2)) <= c && c <= uchar(*p))
+                return sig;
+        } else if (uchar(*p) == c) return sig;
+    }
+    return !sig;
+}
+
+
+static int singlematch(MatchState *ms, const char *s, const char *p,
+                       const char *ep) {
+    if (s >= ms->src_end)
+        return 0;
+    else {
+        int c = uchar(*s);
+        switch (*p) {
+            case '.':
+                return 1;  /* matches any char */
+            case CHAR_ESC:
+                return match_class(c, uchar(*(p + 1)));
+            case '[':
+                return matchbracketclass(c, p, ep - 1);
+            default:
+                return (uchar(*p) == c);
+        }
+    }
+}
+
+
+static const char *matchbalance(MatchState *ms, const char *s,
+                                const char *p) {
+    if (p >= ms->p_end - 1)
+        janet_panic("malformed pattern (missing arguments to '%%b')");
+    if (*s != *p) return NULL;
+    else {
+        int b = *p;
+        int e = *(p + 1);
+        int cont = 1;
+        while (++s < ms->src_end) {
+            if (*s == e) {
+                if (--cont == 0) return s + 1;
+            } else if (*s == b) cont++;
+        }
+    }
+    return NULL;  /* string ends out of balance */
+}
+
+
+static const char *max_expand(MatchState *ms, const char *s,
+                              const char *p, const char *ep) {
+    ptrdiff_t i = 0;  /* counts maximum expand for item */
+    while (singlematch(ms, s + i, p, ep))
+        i++;
+    /* keeps trying to match with the maximum repetitions */
+    while (i >= 0) {
+        const char *res = match(ms, (s + i), ep + 1);
+        if (res) return res;
+        i--;  /* else didn't match; reduce 1 repetition to try again */
+    }
+    return NULL;
+}
+
+
+static const char *min_expand(MatchState *ms, const char *s,
+                              const char *p, const char *ep) {
+    for (;;) {
+        const char *res = match(ms, s, ep + 1);
+        if (res != NULL)
+            return res;
+        else if (singlematch(ms, s, p, ep))
+            s++;  /* try with one more repetition */
+        else return NULL;
+    }
+}
+
+
+static const char *start_capture(MatchState *ms, const char *s,
+                                 const char *p, int what) {
+    const char *res;
+    int level = ms->level;
+    if (level >= MAXCAPTURES) janet_panic("too many captures");
+    ms->capture[level].init = s;
+    ms->capture[level].len = what;
+    ms->level = level + 1;
+    if ((res = match(ms, s, p)) == NULL) /* match failed? */
+        ms->level--;  /* undo capture */
+    return res;
+}
+
+
+static const char *end_capture(MatchState *ms, const char *s,
+                               const char *p) {
+    int l = capture_to_close(ms);
+    const char *res;
+    ms->capture[l].len = s - ms->capture[l].init;  /* close capture */
+    if ((res = match(ms, s, p)) == NULL)  /* match failed? */
+        ms->capture[l].len = CAP_UNFINISHED;  /* undo capture */
+    return res;
+}
+
+
+static const char *match_capture(MatchState *ms, const char *s, int l) {
+    size_t len;
+    l = check_capture(ms, l);
+    len = ms->capture[l].len;
+    if ((size_t)(ms->src_end - s) >= len &&
+            memcmp(ms->capture[l].init, s, len) == 0)
+        return s + len;
+    else return NULL;
+}
+
+
+static const char *match(MatchState *ms, const char *s, const char *p) {
+    if (ms->matchdepth-- == 0)
+        janet_panic("pattern too complex");
+init: /* using goto's to optimize tail recursion */
+    if (p != ms->p_end) {  /* end of pattern? */
+        switch (*p) {
+            case '(': {  /* start capture */
+                if (*(p + 1) == ')')  /* position capture? */
+                    s = start_capture(ms, s, p + 2, CAP_POSITION);
+                else
+                    s = start_capture(ms, s, p + 1, CAP_UNFINISHED);
+                break;
+            }
+            case ')': {  /* end capture */
+                s = end_capture(ms, s, p + 1);
+                break;
+            }
+            case '$': {
+                if ((p + 1) != ms->p_end)  /* is the '$' the last char in pattern? */
+                    goto dflt;  /* no; go to default */
+                s = (s == ms->src_end) ? s : NULL;  /* check end of string */
+                break;
+            }
+            case CHAR_ESC: {  /* escaped sequences not in the format class[*+?-]? */
+                switch (*(p + 1)) {
+                    case 'b': {  /* balanced string? */
+                        s = matchbalance(ms, s, p + 2);
+                        if (s != NULL) {
+                            p += 4;
+                            goto init;  /* return match(ms, s, p + 4); */
+                        }  /* else fail (s == NULL) */
+                        break;
+                    }
+                    case 'f': {  /* frontier? */
+                        const char *ep;
+                        char previous;
+                        p += 2;
+                        if (*p != '[')
+                            janet_panic("missing '[' after '%%f' in pattern");
+                        ep = classend(ms, p);  /* points to what is next */
+                        previous = (s == ms->src_init) ? '\0' : *(s - 1);
+                        if (!matchbracketclass(uchar(previous), p, ep - 1) &&
+                                matchbracketclass(uchar(*s), p, ep - 1)) {
+                            p = ep;
+                            goto init;  /* return match(ms, s, ep); */
+                        }
+                        s = NULL;  /* match failed */
+                        break;
+                    }
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {  /* capture results (%0-%9)? */
+                        s = match_capture(ms, s, uchar(*(p + 1)));
+                        if (s != NULL) {
+                            p += 2;
+                            goto init;  /* return match(ms, s, p + 2) */
+                        }
+                        break;
+                    }
+                    default:
+                        goto dflt;
+                }
+                break;
+            }
+            default:
+            dflt: {  /* pattern class plus optional suffix */
+                    const char *ep = classend(ms, p);  /* points to optional suffix */
+                    /* does not match at least once? */
+                    if (!singlematch(ms, s, p, ep)) {
+                        if (*ep == '*' || *ep == '?' || *ep == '-') {  /* accept empty? */
+                            p = ep + 1;
+                            goto init;  /* return match(ms, s, ep + 1); */
+                        } else /* '+' or no suffix */
+                            s = NULL;  /* fail */
+                    } else { /* matched once */
+                        switch (*ep) {  /* handle optional suffix */
+                            case '?': {  /* optional */
+                                const char *res;
+                                if ((res = match(ms, s + 1, ep + 1)) != NULL)
+                                    s = res;
+                                else {
+                                    p = ep + 1;
+                                    goto init;  /* else return match(ms, s, ep + 1); */
+                                }
+                                break;
+                            }
+                            case '+':  /* 1 or more repetitions */
+                                s++;  /* 1 match already done */
+                            /* FALLTHROUGH */
+                            case '*':  /* 0 or more repetitions */
+                                s = max_expand(ms, s, p, ep);
+                                break;
+                            case '-':  /* 0 or more repetitions (minimum) */
+                                s = min_expand(ms, s, p, ep);
+                                break;
+                            default:  /* no suffix */
+                                s++;
+                                p = ep;
+                                goto init;  /* return match(ms, s + 1, ep); */
+                        }
+                    }
+                    break;
+                }
+        }
+    }
+    ms->matchdepth++;
+    return s;
+}
+
+
+static void push_onecapture(MatchState *ms, int i, const char *s,
+                            const char *e, JanetArray *captures) {
+    if (i >= ms->level) {
+        if (i == 0)  /* ms->level == 0, too */
+            janet_array_push(captures, janet_wrap_string(janet_string((uint8_t *)s, e - s))); /* add whole match */
+        else
+            janet_panicf("invalid capture index %%%d", i + 1);
+    } else {
+        ptrdiff_t l = ms->capture[i].len;
+        if (l == CAP_UNFINISHED) janet_panic("unfinished capture");
+        if (l == CAP_POSITION)
+            janet_array_push(captures, janet_wrap_number((ms->capture[i].init - ms->src_init) + 1));
+        else
+            janet_array_push(captures, janet_wrap_string(janet_string((uint8_t *)(ms->capture[i].init), l)));
+    }
+}
+
+
+static int push_captures(MatchState *ms, const char *s, const char *e, JanetArray *captures) {
+    int i;
+    int nlevels = (ms->level == 0 && s) ? 1 : ms->level;
+    for (i = 0; i < nlevels; i++)
+        push_onecapture(ms, i, s, e, captures);
+    return nlevels;  /* number of strings pushed */
+}
+
+
+
+static void prepstate(MatchState *ms,
+                      const char *s, size_t ls, const char *p, size_t lp) {
+    ms->matchdepth = MAXCCALLS;
+    ms->src_init = s;
+    ms->src_end = s + ls;
+    ms->p_end = p + lp;
+}
+
+
+static void reprepstate(MatchState *ms) {
+    ms->level = 0;
+    if (ms->matchdepth != MAXCCALLS) janet_panic("matchdepth error");
+}
+
+
+static size_t posrelatI(int pos, size_t len) {
+    if (pos > 0)
+        return (size_t)pos;
+    else if (pos == 0)
+        return 1;
+    else if (pos < -(int)len)  /* inverted comparison */
+        return 1;  /* clip to 1 */
+    else return len + (size_t)pos + 1;
+}
+
+static Janet cfun_str_match(int32_t argc, Janet *argv) {
+    janet_arity(argc, 2, 3);
+    JanetByteView view = janet_getbytes(argv, 0);
+    const char *s = (const char *) view.bytes;
+    size_t ls = (size_t) view.len;
+    const char *p = (const char *) janet_getstring(argv, 1);
+    size_t lp = janet_string_length(p);
+    size_t init = 0;
+    if (argc == 3) {
+        init = posrelatI(janet_getinteger(argv, 2), ls) - 1;
+    }
+    if (init > ls) {  /* start after string's end? */
+        return janet_wrap_nil();  /* cannot find anything */
+    }
+    MatchState ms;
+    const char *s1 = s + init;
+    int anchor = (*p == '^');
+    if (anchor) {
+        p++;
+        lp--;  /* skip anchor character */
+    }
+    prepstate(&ms, s, ls, p, lp);
+    do {
+        const char *res;
+        reprepstate(&ms);
+        if ((res = match(&ms, s1, p)) != NULL) {
+            JanetArray *captures = janet_array(8);
+            push_captures(&ms, s1, res, captures);
+            return janet_wrap_array(captures);
+        }
+    } while (s1++ < ms.src_end && !anchor);
+
+    return janet_wrap_nil();  /* not found */
+}
+
 static const JanetReg string_cfuns[] = {
     {
         "string/slice", cfun_string_slice,
@@ -543,6 +987,12 @@ static const JanetReg string_cfuns[] = {
         JDOC("(string/format format & values)\n\n"
              "Similar to snprintf, but specialized for operating with janet. Returns "
              "a new string.")
+    },
+    {
+        "string/match", cfun_str_match,
+        JDOC("(string/match string pattern [start=1])\n\n"
+             "return array with captures or nil.\n"
+             "(lua style pattern matching)")
     },
     {NULL, NULL, NULL}
 };
